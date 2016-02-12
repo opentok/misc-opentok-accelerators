00fdd1208f4f962b27ddbab2782afae7f983889a
marcopacheco <marcopache@gmail.com>
2016-02-11 22:11:58 -0600

    Text Chat version UI
    This component only implements the needed UI for a text chat interaction
    on a web page.

---
+++ js-text-chat/opentok.js-text-chat/src/opentok-text-chat.js
@@ -0,0 +1,17 @@
+define([
+  // 'Chat',
+  'ChatUI',
+  // 'ChatMessage',
+  // 'ChatWidget',
+], function (ChatUI) {
+
+  window.OTSolution = window.OTSolution || {};
+
+  window.OTSolution.TextChat = {
+    // Chat: Chat,
+    ChatUI: ChatUI,
+    // ChatMessage: ChatMessage,
+    // ChatWidget: ChatWidget,
+  };
+
+});
