ef7d70b11ccec71c1cb5318e3be654a9c420c27b
Lucas Huang <lucas@tokbox.com>
2016-03-30 16:15:11 -0700

    combine TextChatKit and TextChatSampleApp for easy development;

---
+++ iOS-text-chat/TextChatSampleApp/TextChatKit/TextChatKit.h
@@ -0,0 +1,22 @@
+//
+//  TextChatKit.h
+//  TextChatKit
+//
+//  Created by Xi Huang on 3/30/16.
+//  Copyright © 2016 Lucas Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextChatKit.
+FOUNDATION_EXPORT double TextChatKitVersionNumber;
+
+//! Project version string for TextChatKit.
+FOUNDATION_EXPORT const unsigned char TextChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextChatKit/PublicHeader.h>
+
+
+#import <TextChatKit/TextChatComponent.h>
+#import <TextChatKit/TextChatComponentChatView.h>
+#import <TextChatKit/TextChatComponentTableViewCell.h>
