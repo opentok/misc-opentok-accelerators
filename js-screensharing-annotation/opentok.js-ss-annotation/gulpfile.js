4c56a93e643f319923af31a8e769050945639155
Aaron Rice <adrice727@gmail.com>
2016-04-15 13:36:05 -0700

    Add 'default' gulp task.

--- js-screensharing-annotation/opentok.js-ss-annotation/gulpfile.js
+++ js-screensharing-annotation/opentok.js-ss-annotation/gulpfile.js
@@ -4,9 +4,11 @@ var importCss = require('gulp-import-css');
 var uglify = require('gulp-uglify');
 inlineCss = require('gulp-inline-css');
 
+gulp.task('default', ['js', 'css']);
+
 gulp.task('js', function () {
   return gulp.src('src/*.js')
-    .pipe(concat('screensharing-annoation.js'))
+    .pipe(concat('screensharing-annotation.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
