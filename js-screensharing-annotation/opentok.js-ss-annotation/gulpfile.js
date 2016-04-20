384e983fa833c15dbc351d9158a072a563cc2142
Aaron Rice <adrice727@gmail.com>
2016-04-20 15:29:37 -0700

    Add dev task.

--- js-screensharing-annotation/opentok.js-ss-annotation/gulpfile.js
+++ js-screensharing-annotation/opentok.js-ss-annotation/gulpfile.js
@@ -5,11 +5,18 @@ var uglify = require('gulp-uglify');
 inlineCss = require('gulp-inline-css');
 
 gulp.task('default', ['js', 'css']);
+gulp.task('dev', ['js-dev', 'css']);
 
 gulp.task('js', function () {
   return gulp.src('src/*.js')
     .pipe(concat('screenshare-annotation-acc-pack.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js-dev', function () {
+  return gulp.src('src/*.js')
+    .pipe(concat('screenshare-annotation-acc-pack.js'))
     .pipe(gulp.dest('dist'));
 });
 
