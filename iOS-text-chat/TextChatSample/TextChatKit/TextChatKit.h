0df62aaf94af72481382c26155ba128e3e7d1881
Lucas Huang <lucas@tokbox.com>
2016-05-03 14:57:29 -0700

    fix WMS282: Text Chat Accelerator toolbar UI inconsistent

--- iOS-text-chat/TextChatSample/TextChatKit/TextChatKit.h
+++ iOS-text-chat/TextChatSample/TextChatKit/TextChatKit.h
@@ -6,16 +6,6 @@
 //  Copyright © 2016 Tokbox. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for TextChatKit.
-FOUNDATION_EXPORT double TextChatKitVersionNumber;
-
-//! Project version string for TextChatKit.
-FOUNDATION_EXPORT const unsigned char TextChatKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TextChatKit/PublicHeader.h>
-
 #import <TextChatKit/TextChatView.h>
 #import <TextChatKit/TextChatUICustomizator.h>
 #import <TextChatKit/OneToOneCommunicator.h>
