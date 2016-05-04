246eb83e9e4a5267123078137b19cd65933e6f30
acn <acn@MacBookCapitanUK.local>
2016-05-04 11:59:35 +0200

    Commented OneToOne tests

--- android-text-chat/TextChatSample/src/test/java/com/tokbox/android/textchatsample/OneToOneCommunicationTest.java
+++ android-text-chat/TextChatSample/src/test/java/com/tokbox/android/textchatsample/OneToOneCommunicationTest.java
@@ -14,111 +14,111 @@ import java.lang.reflect.Modifier;
 public class OneToOneCommunicationTest{
 
     //Assert init
-    @Test
-    public void init_Test_When_OK() throws Exception {
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(OpenTokConfig.class.getField("SESSION_ID"), OpenTokConfig.class.getField("SESSION_ID").getModifiers() & ~Modifier.FINAL);
-
-        OpenTokConfig.class.getField("SESSION_ID").set(null, "here the session  ID");
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_API_Key_Null() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Session_ID_Null() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Token_Null() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_API_Key_Empty() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Session_ID_Empty() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Token_Empty() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_API_Key_Not_Valid() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Session_ID_Not_Valid() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
-
-    @Test
-    public void init_Test_When_Token_Not_Valid() throws Exception {
-
-        MockContext context = new MockContext();
-
-        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
-
-
-    }
+//    @Test
+//    public void init_Test_When_OK() throws Exception {
+//
+//        Field modifiersField = Field.class.getDeclaredField("modifiers");
+//        modifiersField.setAccessible(true);
+//        modifiersField.setInt(OpenTokConfig.class.getField("SESSION_ID"), OpenTokConfig.class.getField("SESSION_ID").getModifiers() & ~Modifier.FINAL);
+//
+//        OpenTokConfig.class.getField("SESSION_ID").set(null, "here the session  ID");
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_API_Key_Null() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Session_ID_Null() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Token_Null() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_API_Key_Empty() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Session_ID_Empty() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Token_Empty() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_API_Key_Not_Valid() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Session_ID_Not_Valid() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
+//
+//    @Test
+//    public void init_Test_When_Token_Not_Valid() throws Exception {
+//
+//        MockContext context = new MockContext();
+//
+//        OneToOneCommunication oneToOneCommunication = new OneToOneCommunication(context);
+//
+//
+//    }
 
     //Assert start
     //Assert end
