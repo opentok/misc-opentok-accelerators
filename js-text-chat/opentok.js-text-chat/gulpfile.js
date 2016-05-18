56ce496e177bb9bb7e0e812c09a8d4aacf313036
Aaron Rice <adrice727@gmail.com>
2016-05-18 09:32:33 -0700

    Remove unused module.

--- js-text-chat/opentok.js-text-chat/gulpfile.js
+++ js-text-chat/opentok.js-text-chat/gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var importCss = require('gulp-import-css');
 var uglify = require('gulp-uglify');
-inlineCss = require('gulp-inline-css');
 
 gulp.task('js', function () {
   return gulp.src('src/*')
