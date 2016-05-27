290260aa8516d9207f5c39ebd31104e22ad8bc99
Esteban Cordero <esteban@agilityfeat.com>
2016-05-27 08:03:15 -0600

    adding the necesary changes to wire SS and Annotation toolbar

---
+++ iOS/OneToOneScreenShareSample/OneToOneScreenShareSample/ScreenShareKit.framework/Headers/ScreenShareKit.h
@@ -0,0 +1,11 @@
+//
+//  ScreenShareKit.h
+//  ScreenShareKit
+//
+//  Created by Xi Huang on 4/26/16.
+//  Copyright © 2016 Lucas Huang. All rights reserved.
+//
+
+#import <ScreenShareKit/ScreenCapture.h>
+#import <ScreenShareKit/ScreenShareView.h>
+#import <ScreenShareKit/ScreenShareToolbarView.h>
