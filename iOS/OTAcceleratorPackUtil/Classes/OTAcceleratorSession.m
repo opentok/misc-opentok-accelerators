9ac37f80dd2a30d9f25aec05bd0b8f5157e335b8
Lucas Huang <lucas@tokbox.com>
2016-05-27 14:10:11 -0700

    fix a stupid small bug!

--- iOS/OTAcceleratorPackUtil/Classes/OTAcceleratorSession.m
+++ iOS/OTAcceleratorPackUtil/Classes/OTAcceleratorSession.m
@@ -29,7 +29,7 @@ static NSString * InternalToken = @"";
 }
 
 - (NSString *)sessionId {
-    return InternalToken;
+    return InternalSessionId;
 }
 
 + (NSSet<id<OTSessionDelegate>> *)getRegisters {
