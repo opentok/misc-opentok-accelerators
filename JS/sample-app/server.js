717b986e649ab605da311a28efa3ec5b4d648dce
Aaron Rice <adrice727@gmail.com>
2016-06-15 13:11:42 -0700

    Add multiple routes with different sessions.

--- JS/sample-app/server.js
+++ JS/sample-app/server.js
@@ -1,25 +1,35 @@
 /*
  * Express Dependencies
  */
-var express = require('express');
-
-var app = express();
-var port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.use(express.static([__dirname, '/public'].join('')));
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
 
 /*
  * Routes
  */
-app.get('/', function (req, res) {
-  res.render('index.html');
+const api = require('./services/api');
+app.get('/', (req, res) => {
+  const credentials = api.getCredentials(0);
+  res.render('index', { credentials: JSON.stringify(credentials) });
 });
 
-app.get('/google721749a8bd473661.html', function (req, res) {
-  res.render('google721749a8bd473661.html');
+app.get('/google721749a8bd473661.html', (req, res) => {
+  res.render('google721749a8bd473661');
 });
 
-app.get('*', function (req, res) {
+app.get('/:session', (req, res) => {
+  const session = req.params.session;
+  const credentials = api.getCredentials(session);
+  res.render('index', { credentials: JSON.stringify(credentials) });
+});
+
+
+app.get('*', (req, res) => {
   res.redirect('/');
 });
 
