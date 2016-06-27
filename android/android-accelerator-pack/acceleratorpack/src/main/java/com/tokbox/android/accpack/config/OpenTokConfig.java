d632ba2cedc8cfda5721d49a31396d5cce291223
marinas <marinaserranomontes@gmail.com>
2016-06-27 23:24:05 +0200

    Adding onetoonecommunication in Android

---
+++ android/android-accelerator-pack/acceleratorpack/src/main/java/com/tokbox/android/accpack/config/OpenTokConfig.java
@@ -0,0 +1,17 @@
+package com.tokbox.android.accpack.config;
+
+public class OpenTokConfig {
+
+    // For internal use only. Please do not modify or remove this code.
+    public static final String LOG_CLIENT_VERSION = "android-vsol-1.0.0";
+    public static final String LOG_COMPONENTID = "oneToOneCommunication";
+    public static final String LOG_ACTION_INITIALIZE = "Init";
+    public static final String LOG_ACTION_START_COMM = "StartComm";
+    public static final String LOG_ACTION_END_COMM = "EndComm";
+
+    public static final String LOG_VARIATION_ATTEMPT = "Attempt";
+    public static final String LOG_VARIATION_ERROR = "Failure";
+    public static final String LOG_VARIATION_SUCCESS = "Success";
+
+
+}
