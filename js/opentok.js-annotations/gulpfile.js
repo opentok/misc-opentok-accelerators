b0f41f9f726b90fc208a6fc4877e0bac2eae194c
Aaron Rice <adrice727@gmail.com>
2016-07-11 12:47:51 -0700

    Update gulpfile.

--- js/opentok.js-annotations/gulpfile.js
+++ js/opentok.js-annotations/gulpfile.js
@@ -4,40 +4,42 @@ var importCss = require('gulp-import-css');
 var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
 
+var dist = 'dist';
+
 gulp.task('default', ['js', 'css']);
-gulp.task('dev', ['js-dev', 'css']);
 
 gulp.task('js', function () {
-  return gulp.src('src/*.js')
+  return gulp.src(['src/annotation-widget.js', 'src/annotation-acc-pack.js'])
     .pipe(concat('opentok-annotations.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(uglify())
+    .pipe(gulp.dest(dist));
 });
 
 
 gulp.task('css', function () {
   return gulp.src('css/annotation.css')
     .pipe(importCss())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dist));
 });
 
 
 gulp.task('images', function () {
   return gulp.src(
     [
-      'images/**'
+      'images/**',
     ], { base: 'images/' })
- .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('zip', function() {
+gulp.task('zip', function () {
   return gulp.src(
-        [
-         "dist/annotation.css",
-         "dist/images/**",
-         "dist/opentok-annotations.js"
-        ], { base: 'dist/' })
-        .pipe(zip('opentok-js-annotations-1.0.0.zip'))
-        .pipe(gulp.dest('dist'));
-})
+    [
+      'dist/annotation.css',
+      'dist/images/**',
+      'dist/opentok-annotations.js',
+    ], { base: 'dist/' })
+  .pipe(zip('opentok-js-annotations-1.0.0.zip'))
+  .pipe(gulp.dest(dist));
+});
 
 gulp.task('dist', ['js', 'css', 'images', 'zip']);
