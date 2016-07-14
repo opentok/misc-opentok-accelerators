9449bf8c56a4479a3c66771851875f62ee70bb4c
Aaron Rice <adrice727@gmail.com>
2016-07-14 16:28:25 -0700

    Update build process to use NPM.

--- JS/opentok.js-ss-annotation/gulpfile.js
+++ JS/opentok.js-ss-annotation/gulpfile.js
@@ -1,31 +1,39 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var importCss = require('gulp-import-css');
+// var importCss = require('gulp-import-css');
 var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
-var unzip = require('gulp-unzip');
+// var unzip = require('gulp-unzip');
 var concatCss = require('gulp-concat-css');
 
 
 gulp.task('default', ['js', 'css']);
 gulp.task('dev', ['js-dev', 'css']);
 
+
+var jsSourceFiles = [
+  'src/opentok-solutions-logging.js',
+  'src/opentok-one-to-one-communication.js',
+  'src/opentok-screen-sharing.js',
+  'src/opentok-annotation.js',
+];
+
 gulp.task('js', function () {
-  return gulp.src('src/*.js')
+  return gulp.src(jsSourceFiles)
     .pipe(concat('screenshare-annotation-acc-pack.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js-dev', function () {
-  return gulp.src('src/*.js')
+  return gulp.src(jsSourceFiles)
     .pipe(concat('screenshare-annotation-acc-pack.js'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('css', function () {
   return gulp.src('css/*.css')
-    .pipe(concatCss("theme.css"))
+    .pipe(concatCss('theme.css'))
     .pipe(gulp.dest('dist/'));
 });
 
@@ -37,22 +45,21 @@ gulp.task('images', function () {
  .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('unzip', function(){
-  gulp.src("./annotations.zip")
-    .pipe(unzip())
-    .pipe(gulp.dest('./annotations'))
-});
+// gulp.task('unzip', function(){
+//   gulp.src("./annotations.zip")
+//     .pipe(unzip())
+//     .pipe(gulp.dest('./annotations'))
+// });
 
-gulp.task('zip', function() {
-  return gulp.src(
-        [
-         "dist/theme.css",
-         "dist/annotation.css",
-         "dist/images/**",
-         "dist/screenshare-annotation-acc-pack.js"
-        ], { base: 'dist/' })
-        .pipe(zip('opentok-js-screenshare-annotation-1.0.0.zip'))
-        .pipe(gulp.dest('dist'));
-})
+gulp.task('zip', function () {
+  return gulp.src([
+    'dist/theme.css',
+    'dist/annotation.css',
+    'dist/images/**',
+    'dist/screenshare-annotation-acc-pack.js'
+  ], { base: 'dist/' })
+    .pipe(zip('opentok-js-screenshare-annotation-1.0.0.zip'))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('dist', ['js', 'css', 'images', 'zip']);
