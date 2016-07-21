30cae9cfac78c64a11a72ddc8e42361179a824d0
Aaron Rice <adrice727@gmail.com>
2016-07-21 13:35:52 -0700

    Update build.

--- js/build.sh
+++ js/build.sh
@@ -25,7 +25,7 @@ then
 	npm i
 	npm update
   cp -v node_modules/opentok-one-to-one-communication/opentok-one-to-one-communication.js src/opentok-one-to-one-communication.js
-	cp -v node_modules/opentok-solutions-logging/opentok-solutions-logging.js src/opentok-solutions-logging.js
+	cp -v node_modules/opentok-solutions-logging/dist/opentok-solutions-logging.js src/opentok-solutions-logging.js
 	gulp dist
 	cd dist
     cp -v text-chat-acc-pack.js $PUBLIC/js/components/text-chat-acc-pack.js
