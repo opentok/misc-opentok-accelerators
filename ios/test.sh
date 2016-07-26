41007574e6f8779cf1908bbec6bdfb1ed93b5dd4
Lucas Huang <xhuang0802@gmail.com>
2016-07-26 12:05:51 -0700

    Update test.sh

--- ios/test.sh
+++ ios/test.sh
@@ -4,6 +4,6 @@ cd ScreenShareAccPackKit/
 
 pod cache clean --all
 pod install
-xcodebuild clean test -workspace "OTScreenShareAccPackKit.xcworkspace" -scheme "OTScreenShareKitTests" -sdk "iphonesimulator9.3" -destination "OS=9.3,name=iPhone 6 Plus" -configuration Debug
+xcodebuild clean test -workspace "OTScreenShareAccPackKit.xcworkspace" -scheme "OTScreenShareKitTests" -sdk "iphonesimulator9.3" -destination "OS=9.3,name=iPhone 6 Plus" -configuration Debug && exit ${PIPESTATUS[0]}
 
 pod spec lint OTScreenShareKit.podspec --use-libraries --allow-warnings --verbose
