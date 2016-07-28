9b1d21d8276990df2702ccc47ac430fcc534077c
Aaron Rice <adrice727@gmail.com>
2016-07-28 09:41:24 -0700

    Update how external dependencies are required.

--- js/opentok.js-screen-sharing/dist/opentok-screen-sharing.js
+++ js/opentok.js-screen-sharing/dist/opentok-screen-sharing.js
@@ -2,17 +2,21 @@
 (function () {
 
   /** Include external dependencies */
-  var _require = function (dependency) {
-    var canRequire = typeof exports === 'object' && require !== undefined;
-    if (canRequire) {
-      return require(dependency);
-    }
-    throw new Error(['Please include', dependency, 'in your project'].join(' '));
-  };
+  var _;
+  var $;
+  var OTKAnalytics;
 
-  var _ = this._ || _require('underscore');
-  var $ = this.$ || _require('jquery');
-  var OTKAnalytics = this.OTKAnalytics || _require('opentok-solutions-logging');
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    /* eslint-disable import/no-unresolved */
+    _ = require('underscore');
+    $ = require('jquery');
+    OTKAnalytics = ('opentok-solutions-logging');
+    /* eslint-enable import/no-unresolved */
+  } else {
+    _ = this._;
+    $ = this.$;
+    OTKAnalytics = this.OTKAnalytics;
+  }
 
   /** Private Variables*/
   var _this; // Reference to instance of ScreenSharingAccPack
