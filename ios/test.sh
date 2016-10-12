54cad163c5044d0059f7ae05ce8283c558c812a6
albertoacn <alberto@tokbox.com>
2016-10-12 22:46:43 +0200

    Updating ios sdk version in test script (#225)
    * Modified build.sh exit status
    
    * Fixing scripts to exit fail in case
    
    * Updated jasmine tests and script
    
    * Updating ios sdk version in test script

--- ios/test.sh
+++ ios/test.sh
@@ -6,8 +6,8 @@ cd TextChatAccPackKit/
 # install cocoapods
 pod cache clean --all
 pod install
-xcodebuild -workspace "OTTextChatAccPackKit.xcworkspace" -scheme "OTTextChatKitBundle" -sdk "iphonesimulator9.3"
-xcodebuild clean test -workspace "OTTextChatAccPackKit.xcworkspace" -scheme "OTTextChatKitTests" -sdk "iphonesimulator9.3" -destination "OS=9.3,name=iPhone 6 Plus" -configuration Debug
+xcodebuild -workspace "OTTextChatAccPackKit.xcworkspace" -scheme "OTTextChatKitBundle" -sdk "iphonesimulator10.0"
+xcodebuild clean test -workspace "OTTextChatAccPackKit.xcworkspace" -scheme "OTTextChatKitTests" -sdk "iphonesimulator10.0" -destination "OS=10.0,name=iPhone 6 Plus" -configuration Debug
 
 # validate cocoapods submission
 pod spec lint OTTextChatKit.podspec --use-libraries --allow-warnings --verbose
